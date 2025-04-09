a97223b1a251424826702f5ca35ecdb3611313fb
Malthe Hoermand <malthehoer@gmail.com>
2025-04-09 12:54:23 +0200

    [update] added 3 more heroes and moved print statement into hero constructor

--- classes/SuperHero.php
+++ classes/SuperHero.php
@@ -15,5 +15,17 @@ class SuperHero {
         $this->weakness = $weakness;
         $this->courage = $courage;
         $this->morale = $morale;
+
+        $this->printProperties();
+    }
+
+    function printProperties (){
+        print ("Egenskaber for ".$this->heroName
+            ."<br>" ."Alias: ".$this->aliasName
+            ."<br>"."Superkræfter: ".$this->superPowers
+            ."<br>"."Svaghed: ".$this->weakness
+            ."<br>"."Natur: ".$this->courage
+            ."<br>"."Motal: ".$this->morale
+            ."<br><br>");
     }
 }
--- index.php
+++ index.php
@@ -8,10 +8,33 @@ $superman = new SuperHero("Superman",
     "Beskytter mennesker",
     "Gør altid alt rigtigt"
 );
-print ("Egenskaber for ".$superman->heroName
-    ."<br>" ."Alias: ".$superman->aliasName
-    ."<br>"."Superkræfter: ".$superman->superPowers
-    ."<br>"."Svaghed: ".$superman->weakness
-    ."<br>"."Natur: ".$superman->courage
-    ."<br>"."Motal: ".$superman->morale);
+
+$batman = new SuperHero("Batman",
+    "Bruce Wayne",
+    "Er en mesterdetektiv, har avanceret teknologi og kamptræning, ekstrem intelligens",
+    "Er kun et menneske – kan blive såret eller dræbt",
+    "Beskytter Gotham City",
+    "Handler ud fra retfærdighed, men bruger ofte sine egne metoder"
+);
+
+$hulk = new SuperHero("Hulk",
+    "Bruce Banner",
+    "Bliver ekstremt stærk når han bliver vred, næsten usårlig i Hulk-form",
+    "Har svært ved at kontrollere sin vrede og kan blive en trussel for andre",
+    "Ønsker at gøre godt, men kæmper med sin indre vrede",
+    "Er i konstant kamp med sin identitet og forsøger at beskytte uskyldige"
+);
+
+$spiderman = new SuperHero("Spider-Man",
+    "Peter Parker",
+    "Kan svinge mellem bygninger med spind, har spindesans, klatrer på vægge, er smidig og hurtig",
+    "Er sårbar som menneske, kan blive såret eller dræbt",
+    "Beskytter New York City og hjælper almindelige mennesker",
+    "Følger mottoet: 'Med stor magt følger stort ansvar'"
+);
+
+
+
+
+
 
